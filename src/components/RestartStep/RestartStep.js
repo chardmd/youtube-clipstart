9910b6254d208eb5d5c814b7627df832330e7abe
chardmd <richard.dimalanta@gmail.com>
2017-07-02 12:27:07 +1000

    minor improvement.

--- src/components/RestartStep/RestartStep.js
+++ src/components/RestartStep/RestartStep.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './RestartStep.css';
 
 class RestartStep extends Component {
     render() {
         return (
-            <div>
+            <div className="RestartStep">
                 <p>
                     Thank you for using Youtube Clip Start. <br /><br />
                     <a href="#" onClick={this.props.restart}>Click here</a> to try again.
