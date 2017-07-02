8d1be341c7763c7825fc8625c246b020c7e4feec
chardmd <richard.dimalanta@gmail.com>
2017-07-02 12:23:39 +1000

    improve timeSelector appearance.

--- src/components/CopyLink/CopyLink.js
+++ src/components/CopyLink/CopyLink.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import './CopyLink.css';
 
 class CopyLink extends Component {
     render() {
         return (
-            <div>
+            <div className="CopyLink">
                 <TextField disabled={true} value={this.props.value} />
-                <RaisedButton label="Copy" secondary={true} />
+                <RaisedButton label="Copy" secondary={true} className="copyButton" />
             </div>
         );
     }
--- src/components/TimeSelector/TimeSelector.js
+++ src/components/TimeSelector/TimeSelector.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import TextField from 'material-ui/TextField';
+import './TimeSelector.css';
 
 class TimeSelector extends Component {
+
     render() {
+
         return (
-            <div>
-                <TextField hintText="1" floatingLabelText="Enter Minutes" onChange={this.props.changeMinutes} />
-                <TextField hintText="20" floatingLabelText="Enter Seconds" onChange={this.props.changeSeconds} />
+            <div className="TimeSelector">
+                <TextField hintText="1" floatingLabelText="Enter Minutes" onChange={this.props.changeMinutes} className="textField" />
+                <TextField hintText="20" floatingLabelText="Enter Seconds" onChange={this.props.changeSeconds} className="textField" />
             </div>
         );
     }
