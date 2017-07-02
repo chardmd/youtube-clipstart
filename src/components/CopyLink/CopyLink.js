71ac17a743e6b14be6366f1932846f6336aabc1c
chardmd <richard.dimalanta@gmail.com>
2017-07-02 13:46:11 +1000

    copy to clipboard functionality.

--- src/components/CopyLink/CopyLink.js
+++ src/components/CopyLink/CopyLink.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import CopyToClipboard from 'react-copy-to-clipboard';
 import './CopyLink.css';
 
 class CopyLink extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            copied: false,
+        };
+        this.onCopy = this.onCopy.bind(this);
+    }
+
+    onCopy() {
+        this.setState({ copied: true })
+    }
+
     render() {
         return (
             <div className="CopyLink">
-                <TextField disabled={true} value={this.props.value} id="text-link"/>
-                <RaisedButton label="Copy" secondary={true} className="copyButton" />
+                <TextField disabled={true} value={this.props.value} id="text-link" />
+                <CopyToClipboard text={this.props.value} onCopy={this.onCopy}>
+                    <RaisedButton label="Copy" secondary={true} className="copyButton" />
+                </CopyToClipboard>
+                <span className="copied">{this.state.copied ? 'Copied.' : null}</span>
             </div>
         );
     }
